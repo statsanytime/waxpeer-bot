f940e54bfcad5dea16f1351c844790713ccd2398
Pontus Persson <39442866+PixelBoii@users.noreply.github.com>
2023-02-15 10:57:32 +0100

    fix: catch errors when sending out discord notifications so that entire app does not exit because of it

--- src/notifications.ts
+++ src/notifications.ts
@@ -2,9 +2,14 @@ import axios from 'axios';
 import 'dotenv/config';
 
 export async function sendNotification(content: string) {
-    await axios.post(process.env.DISCORD_WEBHOOK_URL, {
-        content,
-    });
+    try {
+        await axios.post(process.env.DISCORD_WEBHOOK_URL, {
+            content,
+        });
+    } catch (err) {
+        console.log('Error sending notification to Discord');
+        console.error(err);
+    }
 
     console.log(content);
 };
